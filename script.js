2f72c0c37efc1d97ea02525bc9fa2a7e5617d3bd
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-31 10:38:10 +0000

    make game over function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const gameBoard = (function (){
         'use strict'
         // 9 squares of board
-        let _board = {
+        const _board = {
             1 : '',
             2 : '',
             3 : '',
@@ -14,10 +14,27 @@ document.addEventListener('DOMContentLoaded', ()=>{
             9 : ''
         }
 
-        let updateBoard = function(choice, slot){
+        const _gameOver = function(){
+            let result = ""
+            let used = 0
+            for(let key in _board){
+                // below fills all blocks
+                // _board[key] = "X"
+                if(!(_board[key] == "")){
+                    used++
+                }
+                console.log(_board[key])
+            }
+
+            return result = used == 9 ? true : false
+        }
+
+        const updateBoard = function(choice = "x", slot = 1){
             _board[slot] = choice
             console.log(_board)
+            console.log(_gameOver())
         }
+        
         return {updateBoard}
     })()
 
@@ -28,7 +45,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const playerOne = Player("Aaron", "X")
     const playerTwo = Player("Anna", "O")
 
-    choice = prompt("x or o")
-    slot = prompt("1 - 9")
-    gameBoard.updateBoard(choice, slot)
+    // choice = prompt("x or o")
+    // slot = prompt("1 - 9")
+    // gameBoard.updateBoard(choice, slot)
+
+    gameBoard.updateBoard()
 })
