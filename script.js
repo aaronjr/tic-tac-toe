c96d571f28a17e60bd06d3baea100be588f90cdd
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-31 11:48:01 +0000

    create function to take input and stop when board is full

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const gameBoard = (function(){
+    const GameBoard = (function(){
         'use strict'
         // 9 squares of board
         const _board = {
@@ -9,21 +9,38 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         // update board, 
-        const updateBoard = function(player, slot = 1){
+        const updateBoard = function(player, slot){
             _board[slot] = player
         }
-        // check for 9 inputs
+        // check for 9 inputs if "" is present
         const gameOver = function(){
-            let used = 0
-            for(let key in _board){
-                // below fills all blocks
-                // _board[key] = "X"
-                !_board[key] == "" ? used++ : used
-            }
-            return used == 9 ? true : false
+            //for(let key in _board){
+            //    _board[key] = "X"
+            //}
+            console.log(_board)
+            return Object.values(_board).includes("") ? false : true
         }
         
         return {updateBoard, gameOver}
+    })()
+
+    const Flow = (function(){
+        'use strict'
+        let play = (one, two) => {
+            // check if game over - if not accept inputs.
+            // check again in the middle as odd amount of total turns. 
+            while(!GameBoard.gameOver()){
+                let oneChoice = prompt("1 - 9")
+                GameBoard.updateBoard(one, oneChoice)
+                if(GameBoard.gameOver() == true){
+                    break
+                }
+                let twoChoice = prompt("1 - 9")
+                GameBoard.updateBoard(two, twoChoice)
+            }
+            GameBoard.gameOver() == true ? console.log('over') : console.log('how is it here')
+        }
+        return {play}
     })()
 
     // create a player, name and X or O
@@ -39,10 +56,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // choice = prompt("x or o")
     // slot = prompt("1 - 9")
     // gameBoard.updateBoard(choice, slot)
+    // console.log(playerOne.name)
+    // console.log(playerOne.piece)
 
-    console.log(playerOne.name)
-    console.log(playerOne.piece)
-
-    gameBoard.updateBoard(playerOne.name, 6)
-    console.log(gameBoard.gameOver())
+    //GameBoard.updateBoard(playerOne.name, 6)
+    // console.log(GameBoard.gameOver())
+    Flow.play(playerTwo, playerOne)
 })
