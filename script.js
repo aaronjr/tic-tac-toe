91114cd32a4273a6d04a2110e236baa164a5226a
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-31 10:05:59 +0000

    set up of board and basic factory for players

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-   
+    const gameBoard = (function (){
+        'use strict'
+        // 9 squares of board
+        let _board = {
+            1 : '',
+            2 : '',
+            3 : '',
+            4 : '',
+            5 : '',
+            6 : '',
+            7 : '',
+            8 : '',
+            9 : ''
+        }
 
+        let updateBoard = function(choice, slot){
+            _board[slot] = choice
+            console.log(_board)
+        }
+        return {updateBoard}
+    })()
 
+    const Player = (name, piece) => {
+        console.log(name, piece)
+    }
+
+    const playerOne = Player("Aaron", "X")
+    const playerTwo = Player("Anna", "O")
+
+    choice = prompt("x or o")
+    slot = prompt("1 - 9")
+    gameBoard.updateBoard(choice, slot)
 })
